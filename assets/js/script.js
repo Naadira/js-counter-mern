f6c1141bc2711422b004ffc0041266b15d1cd205
Naadira <naadirasahar18@gmail.com>
2024-02-27 20:11:34 +0530

    Counter created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
+let counter = 0;
+
 const incrementFunction = () => {
-    console.log(`increment logged!`)
+    counter++;
+    document.getElementById('counter-number').textContent = counter;
 }
+    console.log(`increment logged!`)
+
 
 const decrementFunction = () => {
-    console.log(`decrement logged!`)
+    counter--;
+    document.getElementById('counter-number').textContent = counter;
 }
+    console.log(`decrement logged!`)
